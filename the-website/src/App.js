9e2056f6cf17dfff071a23dab6889bda84f9c155
BaranKazan <Baran.kazan@hotmail.com>
2020-09-13 16:58:24 +0200

    Created Navbar and router

--- the-website/src/App.js
+++ the-website/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
-import Index from './module/Index'
+import Home from './module/Home'
+import NavBar from './module/NavBar'
+import ContactUs from './module/ContactUs'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Index/>
+    <Router>
+      <NavBar />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/contact-us">
+          <ContactUs />
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
---
+++ the-website/src/module/NavBar.js
@@ -0,0 +1,21 @@
+import {Navbar, Nav} from 'react-bootstrap'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NavBar() {
+    return (
+        <Navbar bg="red" expand="lg">
+            <Navbar.Brand href="/">Orkes Elektronik</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/contact-us">Constact us</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavBar;
