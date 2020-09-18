c0f3fcf83e9fc86162d5d0109daf626fa3313c6b
BaranKazan <Baran.kazan@hotmail.com>
2020-09-18 20:22:56 +0200

    Added validation on every input

--- the-website/src/module/ContactUs.js
+++ the-website/src/module/ContactUs.js
@@ -21,9 +21,14 @@ class ContactUs extends Component {
             subject: '',
             message: '',
 
+            validFirstName: null,
+            validLastName: null,
+            validCompanyName: null,
             validEmail: null,
             validPhoneNumber: null,
             validCountry: null,
+            validSubject: null,
+            validMessage: null,
 
             successMessage: false,
         }
@@ -42,36 +47,39 @@ class ContactUs extends Component {
                     <Form.Row>
                         <Form.Group as={Col} controlId="firstName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control onChange={this.changeFirstName} required />
+                            <Form.Control onChange={this.changeFirstName} isValid={this.state.validFirstName}
+                                isInvalid={this.state.validFirstName === null ? false : !this.state.validFirstName} required />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="secondName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control onChange={this.changeSecondName} required />
+                            <Form.Control onChange={this.changeSecondName} isValid={this.state.validLastName}
+                                isInvalid={this.state.validLastName === null ? false : !this.state.validLastName} required />
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Group controlId="companyName">
                         <Form.Label>Company Name</Form.Label>
-                        <Form.Control onChange={this.changeCompanyName} required />
+                        <Form.Control onChange={this.changeCompanyName} isValid={this.state.validCompanyName}
+                            isInvalid={this.state.validCompanyName === null ? false : !this.state.validCompanyName} required />
                     </Form.Group>
 
                     <Form.Group controlId="email">
                         <Form.Label>E-mail</Form.Label>
-                        <Form.Control onChange={this.changeEmail} isValid={this.state.validEmail} 
-                        isInvalid={this.state.validEmail === null ? false : !this.state.validEmail} required />
+                        <Form.Control onChange={this.changeEmail} isValid={this.state.validEmail}
+                            isInvalid={this.state.validEmail === null ? false : !this.state.validEmail} required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="phoneNumber">
                         <Form.Label>Phone Number</Form.Label>
-                        <Form.Control onChange={this.changePhoneNumber} isValid={this.state.validPhoneNumber} 
-                        isInvalid={this.state.validPhoneNumber === null ? false : !this.state.validPhoneNumber} required />
+                        <Form.Control onChange={this.changePhoneNumber} isValid={this.state.validPhoneNumber}
+                            isInvalid={this.state.validPhoneNumber === null ? false : !this.state.validPhoneNumber} required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="country">
                         <Form.Label>Country</Form.Label>
-                        <Form.Control as="select" defaultValue="Select country..." onChange={this.changeCountry} isValid={this.state.validCountry} 
-                        isInvalid={this.state.validCountry === null ? false : !this.state.validCountry} required>
+                        <Form.Control as="select" defaultValue="Select country..." onChange={this.changeCountry} isValid={this.state.validCountry}
+                            isInvalid={this.state.validCountry === null ? false : !this.state.validCountry} required>
                             <option>Select country...</option>
                             {
                                 this.options.map((value, i) => {
@@ -83,12 +91,14 @@ class ContactUs extends Component {
 
                     <Form.Group as={Col} controlId="subject">
                         <Form.Label>Subject</Form.Label>
-                        <Form.Control onChange={(e) => { this.setState({ subject: e.target.value }) }} required />
+                        <Form.Control onChange={this.changeSubject} isValid={this.state.validSubject}
+                            isInvalid={this.state.validSubject === null ? false : !this.state.validSubject} required />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="message">
                         <Form.Label>Message</Form.Label>
-                        <Form.Control as="textarea" onChange={(e) => { this.setState({ message: e.target.value }) }} required />
+                        <Form.Control as="textarea" onChange={this.changeMessage} isValid={this.state.validMessage}
+                            isInvalid={this.state.validMessage === null ? false : !this.state.validMessage} required />
                     </Form.Group>
                     <Button variant="primary" type="submit" onClick={this.buttonClicked}>
                         Submit
@@ -98,16 +108,6 @@ class ContactUs extends Component {
         );
     }
 
-    numberOnly = (e) => {
-        console.log(e.target.value);
-        const regex = /^[+0-9]?[0-9]+$/;
-        if (regex.test(e.target.value)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     buttonClicked = (e) => {
         e.preventDefault();
         console.log(this.state)
@@ -115,20 +115,32 @@ class ContactUs extends Component {
     }
 
     changeFirstName = (e) => {
-        this.setState({ firstName: e.target.value })
+        if (!(e.target.value === "")) {
+            this.setState({ firstName: e.target.value, validFirstName: true })
+        } else {
+            this.setState({ validFirstName: false })
+        }
     }
 
     changeSecondName = (e) => {
-        this.setState({ lastName: e.target.value })
+        if (!(e.target.value === "")) {
+            this.setState({ lastName: e.target.value, validLastName: true })
+        } else {
+            this.setState({ validLastName: false })
+        }
     }
 
     changeCompanyName = (e) => {
-        this.setState({ companyName: e.target.value })
+        if (!(e.target.value === "")) {
+            this.setState({ companyName: e.target.value, validCompanyName: true })
+        } else {
+            this.setState({ validCompanyName: false })
+        }
     }
 
     changeEmail = (e) => {
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if (emailRegex.test(e.target.value)) {
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (regex.test(e.target.value)) {
             this.setState({ email: e.target.value, validEmail: true });
         } else {
             this.setState({ validEmail: false });
@@ -136,8 +148,8 @@ class ContactUs extends Component {
     }
 
     changePhoneNumber = (e) => {
-        const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/
-        if (phoneRegex.test(e.target.value)) {
+        const regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/
+        if (regex.test(e.target.value)) {
             this.setState({ phoneNumber: e.target.value, validPhoneNumber: true });
         } else {
             this.setState({ validPhoneNumber: false })
@@ -154,11 +166,19 @@ class ContactUs extends Component {
     }
 
     changeSubject = (e) => {
-        this.setState({ subject: e.target.value });
+        if (!(e.target.value === "")) {
+            this.setState({ subject: e.target.value, validSubject: true })
+        } else {
+            this.setState({ validSubject: false })
+        }
     }
 
     changeMessage = (e) => {
-        this.setState({ message: e.target.value })
+        if (!(e.target.value === "")) {
+            this.setState({ message: e.target.value, validMessage: true })
+        } else {
+            this.setState({ validMessage: false })
+        }
     }
 
 }
