115bc8356d69f6ba220f0f72121dc2097851a7b7
BaranKazan <Baran.kazan@hotmail.com>
2020-09-27 21:45:38 +0200

    Changed the design of the layout
    Contact us is not working atm. The design have to be adjusted and then the function can be aplied.

--- Mark 2/src/Components/Header.js
+++ Mark 2/src/Components/Header.js
@@ -3,18 +3,7 @@ import ParticlesBg  from "particles-bg";
 
 class Header extends Component {
   render() {
-
-    if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
-      var name = this.props.data.name;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
-
+     
     return (
       <header id="home">
       <ParticlesBg type="circle" bg={true} />
@@ -25,8 +14,6 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
       </nav>
@@ -34,12 +21,12 @@ class Header extends Component {
       <div className="row banner">
       
          <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
+            <h1 className="responsive-headline">Orkes elektronik</h1>
+            <h3>Hello world.</h3>
             <hr />
             <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+               <a href="#" className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
+               <a href="#" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
       </div>
