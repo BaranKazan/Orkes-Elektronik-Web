a98a153d45ba4dd0b13f34b0c1e9b107a882dc56
BaranKazan <Baran.kazan@hotmail.com>
2020-10-07 21:36:23 +0200

    Almost done
    The webbsite is almost done. The only thing left is the alert message when sumbiting the message.

--- Mark 2/src/App.js
+++ Mark 2/src/App.js
@@ -4,16 +4,24 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 class App extends Component {
 
   render() {
+    const options = {
+      timeout: 5000,
+      position: positions.BOTTOM_CENTER
+    };
     return (
       <div className="App">
-        <Header />
-        <About />
-        <Contact />
-        <Footer />
+        <Provider template={AlertTemplate} {...options}>
+          <Header />
+          <About />
+          <Contact />
+          <Footer />
+        </Provider>
       </div>
     );
   }
--- Mark 2/src/Components/About.js
+++ Mark 2/src/Components/About.js
@@ -7,9 +7,10 @@ class About extends Component {
             <div className="row">
                <div className="nine columns main-col">
                   <h2>About Us</h2>
-                  <p>Hello</p>
-                  <p>there</p>
-                  <p>world</p>
+                  <p>Hello!</p>
+                  <p>We are the Orkes Elektronik, a team of engineers that are capable of creating circuit boards as well as program those circuit boards. 
+                     We are doing integrations tests as well to ensure the product works as intended.</p>
+                  <p>We are creating unmanned aerial vehicle (UAV) with auto-pilot functionality and AES 128 bites encryption to ensure it does not get hijacked.</p>
                </div>
             </div>
 
